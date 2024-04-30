1518b0544658c7c748cb567db83003dbbbca2afe
hasbiafa <76551370+hasbiauzan19@users.noreply.github.com>
2024-04-30 16:32:45 +0700

    Adding image assets, product section, and search bar

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// toggle class active
+// toggle class active hamburger-menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Hamburger menu onclick
@@ -6,10 +6,26 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
-// Klik diluar sidebar untuk menghilangkan navnya
-const hamburger = document.querySelector("#hamburger-menu");
+// toggle class active search-bar
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = () => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault;
+};
+
+// Klik diluar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
